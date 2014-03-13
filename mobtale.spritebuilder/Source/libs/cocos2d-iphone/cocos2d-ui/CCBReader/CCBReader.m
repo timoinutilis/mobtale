059282270b64325754dcb27385b080a1e2b02015
Timo Kloss <timo.kloss@inutilis.com>
2014-03-14 00:26:09 +0100

    updated cocos2d to rc4

--- mobtale.spritebuilder/Source/libs/cocos2d-iphone/cocos2d-ui/CCBReader/CCBReader.m
+++ mobtale.spritebuilder/Source/libs/cocos2d-iphone/cocos2d-ui/CCBReader/CCBReader.m
@@ -656,7 +656,8 @@ static inline float readFloat(CCBReader *self)
                     
                     void (^block)(id sender);
                     block = ^(id sender) {
-                        objc_msgSend(t, selector, sender);
+                        typedef void (*Func)(id, SEL, id);
+                        ((Func)objc_msgSend)(t, selector, sender);
                     };
                     
                     NSString* setSelectorName = [NSString stringWithFormat:@"set%@:",[name capitalizedString]];
@@ -664,7 +665,8 @@ static inline float readFloat(CCBReader *self)
                     
                     if ([node respondsToSelector:setSelector])
                     {
-                        objc_msgSend(node, setSelector, block);
+                        typedef void (*Func)(id, SEL, id);
+                        ((Func)objc_msgSend)(node, setSelector, block);
                     }
                     else
                     {
@@ -883,7 +885,8 @@ static inline float readFloat(CCBReader *self)
         embeddedNode.positionType = ccbFileNode.positionType;
         //embeddedNode.anchorPoint = ccbFileNode.anchorPoint;
         embeddedNode.rotation = ccbFileNode.rotation;
-        embeddedNode.scale = ccbFileNode.scale;
+        embeddedNode.scaleX = ccbFileNode.scaleX;
+        embeddedNode.scaleY = ccbFileNode.scaleY;
         embeddedNode.name = ccbFileNode.name;
         embeddedNode.visible = YES;
         //embeddedNode.ignoreAnchorPointForPosition = ccbFileNode.ignoreAnchorPointForPosition;
